049afc5255868c003b68831870ef0c0342966b2d
Swizec Teller <swizec@swizec.com>
2017-10-04 00:29:10 -0700

    Use React 16

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,43 +2,45 @@ module.exports = {
   siteMetadata: {
     title: `Intro to React workshop by Swizec`,
   },
-  plugins: [`gatsby-plugin-react-helmet`,
-            {
-                resolve: `gatsby-plugin-typography`,
-                options: {
-                    pathToConfigModule: `src/utils/typography.js`
-                }
-            },
-            `gatsby-plugin-styled-components`,
-            {
-                resolve: `gatsby-source-filesystem`,
-                options: {
-                    name: `src`,
-                    path: `${__dirname}/src/`
-                }
-            },
-            {
-                resolve: `gatsby-transformer-remark`,
-                options: {
-                    plugins: [
-                        {
-                            resolve: `gatsby-remark-prismjs`,
-                            options: {
-                                classPrefix: `language-`
-                            }
-                        },
-                        {
-                            resolve: `gatsby-remark-images`,
-                            options: {
-                                maxWidth: 960,
-                                linkImagesToOriginal: true
-                            }
-                        },
-                        `gatsby-remark-copy-linked-files`,
-                        `gatsby-remark-autolink-headers`
-                    ]
-                }
-            }
+  plugins: [
+      `gatsby-plugin-react-next`,
+      `gatsby-plugin-react-helmet`,
+      {
+          resolve: `gatsby-plugin-typography`,
+          options: {
+              pathToConfigModule: `src/utils/typography.js`
+          }
+      },
+      `gatsby-plugin-styled-components`,
+      {
+          resolve: `gatsby-source-filesystem`,
+          options: {
+              name: `src`,
+              path: `${__dirname}/src/`
+          }
+      },
+      {
+          resolve: `gatsby-transformer-remark`,
+          options: {
+              plugins: [
+                  {
+                      resolve: `gatsby-remark-prismjs`,
+                      options: {
+                          classPrefix: `language-`
+                      }
+                  },
+                  {
+                      resolve: `gatsby-remark-images`,
+                      options: {
+                          maxWidth: 960,
+                          linkImagesToOriginal: true
+                      }
+                  },
+                  `gatsby-remark-copy-linked-files`,
+                  `gatsby-remark-autolink-headers`
+              ]
+          }
+      }
   ],
   pathPrefix: `/intro-to-react-workshop`
 }
